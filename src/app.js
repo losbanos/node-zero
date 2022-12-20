f143baf9d31de3e0b34d0493c4cb0da36e226bd7
kimtaehoon-mn <kimtaehoon@smilegate.com>
2022-12-20 09:28:11 +0900

    [http] write response

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html lang="ko">');
+    res.write('<head><title>Ted Jin 520</title></head>');
+    res.write('<body><h1>This is Node Server Page</h1></body>');
+    res.write('</html>');
     res.end('Ted, it\'s ok');
+    // process.exit();
 });
 
 server.listen(4000);
