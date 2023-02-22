53d4fd2c942135843b1a50510c8478a4db25af26
kimtaehoon-mn <kimtaehoon@smilegate.com>
2023-02-22 12:23:07 +0900

    [Node] 404 html 페이지 서비스 하기

--- src/routes/404.js
+++ src/routes/404.js
@@ -1,5 +1,5 @@
 const express = require('express');
+const path = require('path');
 module.exports = express.Router().use((req, res, next) => {
-    res.status(404);
-    res.send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname, '../', 'views', '404.html'));
 })
