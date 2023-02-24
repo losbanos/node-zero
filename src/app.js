e7994ecdf5264e750c709ae218aafe916647ffeb
kimtaehoon-mn <kimtaehoon@smilegate.com>
2023-02-24 20:05:03 +0900

    [Node] css 파일 적용 및 분리

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {rootDir} = require('./utils/path');
 const routerAdmin = require('./routes/admin');
 const routerShop = require('./routes/shop');
 const router404 = require('./routes/404');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/admin', routerAdmin);
 app.use(routerShop);
