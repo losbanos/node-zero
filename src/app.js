9141fce44ee5a78df4efd2fbe16d043b1cc20767
kimtaehoon-mn <kimtaehoon@smilegate.com>
2023-02-26 18:13:11 +0900

    [Node] css 파일 적용 및 분리 2

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {rootDir} = require('./utils/path');
+const {publicDir} = require('./utils/path');
 const routerAdmin = require('./routes/admin');
 const routerShop = require('./routes/shop');
 const router404 = require('./routes/404');
-const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(rootDir, 'public')));
-
+app.use(express.static(publicDir));
 app.use('/admin', routerAdmin);
 app.use(routerShop);
 app.use(router404);
--- src/utils/path.js
+++ src/utils/path.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
 const rootDir = path.dirname(process.mainModule.filename);
+const publicDir = path.join(process.env.PWD, 'public');
+
 module.exports = {
-    rootDir
+    rootDir,
+    publicDir
 }
+
