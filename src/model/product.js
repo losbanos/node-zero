848ebf89fa15358400282377b85832286289ad1b
kimtaehoon <kimtaehoon@smilegate.com>
2023-06-22 09:34:49 +0900

    [Node] 파일 IO를 통한 정보 저장/조회 리팩토링

--- src/model/product.js
+++ src/model/product.js
@@ -1,8 +1,17 @@
 const path = require('path');
 const fs = require('fs');
 const {re} = require('@babel/core/lib/vendor/import-meta-resolve');
-const products = [];
 
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, data) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(data));
+        }
+    });
+}
 class Product {
     constructor(title, description) {
         this.title = title;
@@ -10,34 +19,16 @@ class Product {
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, (err, data) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(data);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), err => {
+        getProductsFromFile(productsData => {
+            productsData.push(this);
+            fs.writeFile(p, JSON.stringify(productsData), err => {
                 console.log('error write = ', err);
             })
         })
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        let result = []
-        fs.readFile(p, (err, data) => {
-            if(err) {
-                cb([]);
-            } else {
-                cb(JSON.parse(data));
-            }
-        });
-        return result;
-    }
-
-    retrieveProducts() {
-
+        getProductsFromFile(cb);
     }
 }
 
