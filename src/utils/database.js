80e7bfa5b2f2086d9a9a3e1cd9b6cbfeb9d9746a
kimtaehoon-mn <kimtaehoon@smilegate.com>
2023-10-18 10:00:05 +0900

    [node] mongodb 계정 정보 분리

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,15 +1,21 @@
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
-
+const path = require('path');
+const fs = require('fs');
 const mongoConnect = (callback) => {
-    mongoClient.connect('mongodb+srv://wwwholakr:ZgoGHxDKqHOEG6Uh@tedjin4.t9owtau.mongodb.net/?retryWrites=true&w=majority')
-        .then(client => {
-            console.log('Connected !!');
-            callback(client);
-        })
-        .catch(error => {
-            console.log('Error = ', error);
-        })
+    fs.readFile(path.join(path.dirname(require.main.filename), 'data', 'account.json'), (err, accountData) => {
+        if (!err) {
+            const accountInfo = JSON.parse(accountData);
+            mongoClient.connect(`mongodb+srv://${accountInfo.id}/?retryWrites=true&w=majority`)
+                .then(client => {
+                    console.log('Connected !!');
+                    callback(client);
+                })
+                .catch(error => {
+                    console.log('Error = ', error);
+                })
+            }
+    })
 
 }
 
