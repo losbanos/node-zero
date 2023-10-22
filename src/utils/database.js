bf01a3bf2394d865dc6c10c28ef55ed2641292c3
kimtaehoon <kimtaehoon@smilegate.com>
2023-10-23 08:50:35 +0900

    [node-mongo] 임의 호스트에 대해 몽고디비 접근 허용

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,10 @@ const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
-    mongoClient.connect('mongodb+srv://wwwholakr:ZgoGHxDKqHOEG6Uh@tedjin4.t9owtau.mongodb.net/?retryWrites=true&w=majority')
+    mongoClient.connect('mongodb+srv://wwwholakr:ZgoGHxDKqHOEG6Uh@tedjin4.t9owtau.mongodb.net/?retryWrites=true&w=majority',{
+        tlsAllowInvalidHostnames: true,
+        tlsAllowInvalidCertificates: true
+    })
         .then(client => {
             console.log('Connected !!');
             callback(client);
