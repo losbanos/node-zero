1bf54b91f6972eabce8febeab7c7f5b6edafcafb
kimtaehoon <kimtaehoon@smilegate.com>
2024-02-16 09:08:27 +0900

    [node-mongo] 유저 정보 임의 하드 코딩

--- src/controllers/initData.js
+++ src/controllers/initData.js
@@ -1,10 +1,26 @@
 const Menu = require('../model/nav');
+const {User} = require('../model/user');
 
 module.exports = function(req, res, next) {
     req.currentLanguage = req.acceptsLanguages()[0];
+    User.findById('65c5e79b9f4e794101cfc52f')
+        .then(result => {
+            req.user = new User({
+                userName: result.userName,
+                userId: result.userId,
+                userEmail: result.userEmail,
+                _id: result._id,
+                regDate: result.regDate,
+                cart: result.cart})
+            next();
+        })
+        .catch(e => {
+            console.error(e);
+            next();
+        })
     // const userName = req.cookies.userName;
     // const isLogined = req.cookies.isLogined;
     // new Menu({userName, isLogined});
     // req.menus = Menu;
-        next();
+    //     next();
 }
--- src/model/user.js
+++ src/model/user.js
@@ -1,40 +1,56 @@
 const {getDb} = require('../utils/database');
-const mongodb = require('mongodb');
+const mongodb= require('mongodb');
+const {ObjectId} = mongodb;
 
 class User {
-    constructor(params) {
-        this.userId = null;
-        this._id = null;
+    constructor(params = {userId: null, _id: null, userName: '', userEmail: '', regDate: Date.now(), cart: {items:[]}}) {
+        this.userId = params.userId;
+        this._id = params._id;
         this.userName = params.userName;
         this.userEmail = params.userEmail;
-        this.regDate = Date.now();
+        this.regDate = params.regDate;
+        this.cart = params.cart;
     }
 
     save() {
         const user = getDb().collection('users');
-        let result;
         if (!this.userId) {
             return user.insertOne(this)
                 .then(res => {
                     console.log('save USER', res);
+                    this.userId = this._id = res._id;
+                    return res;
                 })
                 .catch(e => {
                     console.error(e);
+                    return e;
                 })
         } else {
 
         }
     }
 
+    addToCart(product) {
+        const updatedCart = {items: [{...product, quantity: 1}]};
+        return getDb().collection('users')
+            .updateOne({_id: new ObjectId(this._id)}, {$set: {cart: updatedCart}})
+            .then(result => {
+                console.log('add to cart success');
+                return result
+            })
+            .catch(e => e);
+
+    }
     static findById(userId) {
         return getDb()
             .collection('users')
             .find({
-                _id: new mongodb.ObjectId(userId)
+                _id: new ObjectId(userId)
             })
             .next()
             .then(res => {
                 console.log('find use by id');
+                return res;
             })
             .catch(e => {
                 console.error(e)
